877fe3faad895ab1a9298b2b6b1a7be82d1c4405
the-supernova <biswalabhijit04@gmail.com>
2023-05-03 15:34:40 +0530

    Fix #2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,11 @@ import GithubProvider from "next-auth/providers/github";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
 
+const serviceAccount = require("../../../lib/fleet-feedback-firebase-adminsdk-zzuia-9aefeb531b.json")
+
 export default NextAuth({
     adapter: FirestoreAdapter({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        }),
+        credential: cert(serviceAccount),
     }),
     providers: [
         GoogleProvider({
