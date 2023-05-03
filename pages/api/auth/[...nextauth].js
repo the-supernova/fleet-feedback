0c1a61d784995d00e1cc353a8fdf7cc001c68aa4
the-supernova <biswalabhijit04@gmail.com>
2023-05-03 13:33:20 +0530

    🔨 refactor(app): Add Firebase adapter to next-auth
    Instead of creating a firestore instance by myself to store users, used the Firebase Adapter provided by NextAuth to do the work for me

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import serviceJSON from "@/lib/fleet-feedback-firebase-adminsdk-zzuia-9aefeb531b.json"
 
 export default NextAuth({
+    adapter: FirestoreAdapter({
+        credential: cert(serviceJSON),
+    }),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
