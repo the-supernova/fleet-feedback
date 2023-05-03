e4dc788d3b6e915c7dd775d6355473c8270fae7b
the-supernova <biswalabhijit04@gmail.com>
2023-05-03 13:54:37 +0530

    🐛 fix: Fix import issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
-import serviceJSON from "@/lib/fleet-feedback-firebase-adminsdk-zzuia-9aefeb531b.json"
+const serviceJSON = require("../../../lib/fleet-feedback-firebase-adminsdk-zzuia-9aefeb531b.json")
 
 export default NextAuth({
     adapter: FirestoreAdapter({
