d3d90b6ab872c7bcd62454aecc9a0851f5503f54
the-supernova <biswalabhijit04@gmail.com>
2023-05-04 17:47:15 +0530

    🐛 fix: Fix adapter credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,16 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 
 export default NextAuth({
-    adapter: FirestoreAdapter(),
+    adapter: FirestoreAdapter({
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+    }),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
